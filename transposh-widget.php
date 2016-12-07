de3c854415963376234eadda1bab16026c05d568
mikewing94 <michaelwing@Michaels-Mac-Pro.local>
2016-12-07 16:45:22 +0000

    Transposh Widget

---
+++ transposh-widget.php
@@ -0,0 +1,34 @@
+<?php
+$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+echo "<div id='flags'>";
+echo "<a href='.'>";
+echo "<img border='0' alt='English' src='/wp-content/uploads/2016/11/England_flag.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=it'>";
+echo "<img border='0' alt='Italian' src='/wp-content/uploads/2016/11/italian-flag-small.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=hu'>";
+echo "<img border='0' alt='Hungarian' src='/wp-content/uploads/2016/11/Flag_of_Hungary.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=es'>";
+echo "<img border='0' alt='Spanish' src='/wp-content/uploads/2016/11/Spain-Flag.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=bg'>";
+echo "<img border='0' alt='Bulgarian' src='/wp-content/uploads/2016/11/Flag_of_Bulgaria_bordered.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=fr'>";
+echo "<img border='0' alt='French' src='/wp-content/uploads/2016/11/french-flag-graphic.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=nl'>";
+echo "<img border='0' alt='Dutch' src='/wp-content/uploads/2016/11/Flag_of_the_Netherlands.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=de'>";
+echo "<img border='0' alt='German' src='/wp-content/uploads/2016/11/germany.png' width='70' height='50'>";
+echo "</a>";
+echo "<a href='./?lang=pl'>";
+echo "<img border='0' alt='Polish' src='/wp-content/uploads/2016/11/Poland_Large_Flag.png' width='70' height='50'>";
+echo "</a>";
+echo "</div>";
+
+?>
